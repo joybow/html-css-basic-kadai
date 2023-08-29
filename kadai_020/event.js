b2072e9223d314d476e50ee22255b8c2bc9fc8ea
joybow <140361593+joybow@users.noreply.github.com>
2023-08-29 13:41:45 +0900

    Update timeout

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,7 @@
 const btnClick = document.getElementById('btn')
 
 btnClick.addEventListener('click',() =>{
+  setTimeout(()=> {
   document.getElementById('text').innerHTML=('ボタンをクリックしました！');
+  },2000);
 });
